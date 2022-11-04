048da0a9a4a586d4dc3e773fa0b5b10f7fae3748
177boris <lanreboris11@yahoo.com>
2022-11-04 01:35:39 +0000

    fix sam deploy error

---
+++ cloud-resume-challenge/lambdas/get_function.py
@@ -0,0 +1,56 @@
+import json
+import boto3 
+from decimal import Decimal
+
+# import requests
+
+class DecimalEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Decimal):
+      return str(obj)
+    return json.JSONEncoder.default(self, obj)
+
+
+def lambda_handler(event, context):
+    
+
+    dynamodb = boto3.resource('dynamodb')
+    count_table = dynamodb.Table('cloud-resume-challenge') 
+
+
+    try:
+        response = count_table.get_item(
+            Key={"ID" : "VISITOR_COUNT"},
+        )  
+    except Exception as e:
+        print("Error: " + e)
+
+        raise e 
+
+    count_value = response['Item']['visitor_count']
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+    
+
+    return {
+
+        # temporarily disable cors for testing purposes => to be fixed in future versions 
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Access-Control-Allow-Headers": "*",
+        }, 
+        "statusCode": 200,
+        "body": json.dumps({
+            "count": json.dumps(count_value, cls=DecimalEncoder),
+            # "location": ip.text.replace("\n", "")
+        }),
+        
+    }
+    
---
+++ cloud-resume-challenge/lambdas/test_put_function.py
@@ -0,0 +1,50 @@
+from put_function import lambda_handler
+
+import unittest 
+import boto3 
+import botocore 
+from moto import mock_dynamodb2 
+
+@mock_dynamodb2
+class TestPutScript(unittest.TestCase):
+    
+        def setUp(self): 
+            self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+            self.count_table = self.dynamodb.create_table(
+                TableName='cloud-resume-challenge',
+                KeySchema=[
+                    {
+                        'AttributeName': 'ID',
+                        'KeyType': 'HASH'
+                    }
+                ],
+                AttributeDefinitions=[
+                    {
+                        'AttributeName': 'ID',
+                        'AttributeType': 'S'
+                    }
+                ],
+                ProvisionedThroughput={
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5
+                }
+            )
+    
+            self.count_table.put_item(
+                Item={
+                    'ID': 'VISITOR_COUNT',
+                    'visitor_count': 0
+                }
+            )
+    
+        def test_put_script(self):
+            event = {}
+            context = {}
+            response = lambda_handler(event, context)
+    
+            self.assertEqual(response['statusCode'], 200)
+            self.assertEqual(response['body'], '{"count": "1"}')
+
+
+if __name__ == '__main__':
+    unittest.main()
